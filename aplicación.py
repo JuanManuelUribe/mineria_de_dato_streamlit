612861eb5ef83bd245c08a4ddc8aab402d56af42
JuanManuelUribe <139479931+JuanManuelUribe@users.noreply.github.com>
2025-01-28 18:55:01 -0500

    Update aplicación.py

--- aplicación.py
+++ aplicación.py
@@ -37,7 +37,7 @@ def main():
       st.markdown("Imagen clasificada")
       model = load_model()
 
-prediction = model.predict(preprocessed_image.reshaped(1,-1)) # (1, 784)
+prediction = model.predict(preprocessed_image.reshape(1,-1)) # (1, 784)
 st.markdown(f"La imagen fue clasificada como: {prediction}")
 
 if __name__=='__main__':
